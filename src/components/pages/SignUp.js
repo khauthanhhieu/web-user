324d0a4cc251aa3d8d4fffad38ef9843d5468d67
khauthanhhieu <thanhhieu021298@gmail.com>
2020-01-01 22:27:38 +0700

    sửa lỗi import thư viện

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,59 +1,13 @@
 import React, { Component } from 'react';
-import { WithContext } from 'react-tag-input';
-import ReactDOM from 'react-dom';
 
-const KeyCodes = {
-  comma: 188,
-  enter: 13,
-};
-
-const delimiters = [KeyCodes.comma, KeyCodes.enter];
 
 export default class SignUp extends Component {
   constructor() {
     super()
     this.setState({ page: 0 })
-    this.state = {
-      tags: [
-        { id: "Thailand", text: "Thailand" },
-        { id: "India", text: "India" }
-      ],
-      suggestions: [
-        { id: 'USA', text: 'USA' },
-        { id: 'Germany', text: 'Germany' },
-        { id: 'Austria', text: 'Austria' },
-        { id: 'Costa Rica', text: 'Costa Rica' },
-        { id: 'Sri Lanka', text: 'Sri Lanka' },
-        { id: 'Thailand', text: 'Thailand' }
-      ]
-    };
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleAddition = this.handleAddition.bind(this);
-    this.handleDrag = this.handleDrag.bind(this);
-  }
-  handleDelete(i) {
-    const { tags } = this.state;
-    this.setState({
-      tags: tags.filter((tag, index) => index !== i),
-    });
-  }
-  handleAddition(tag) {
-    this.setState(state => ({ tags: [...state.tags, tag] }));
-  }
-
-  handleDrag(tag, currPos, newPos) {
-    const tags = [...this.state.tags];
-    const newTags = tags.slice();
-
-    newTags.splice(currPos, 1);
-    newTags.splice(newPos, 0, tag);
-
-    // re-render
-    this.setState({ tags: newTags });
   }
 
   render() {
-    const { tags, suggestions } = this.state;
 
     const now = new Date()
     const defaultBirth = new Date(now.getFullYear() - 18, now.getMonth() + 1, now.getDate()).toISOString().substr(0, 10)
@@ -138,12 +92,7 @@ export default class SignUp extends Component {
 
     const teacherPage = (
       <>
-        <WithContext tags={tags}
-                    suggestions={suggestions}
-                    handleDelete={this.handleDelete}
-                    handleAddition={this.handleAddition}
-                    handleDrag={this.handleDrag}
-                    delimiters={delimiters} />
+
       </>
     )
 
