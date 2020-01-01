895a51bc75749750cb40cfb9f1a4af230856570b
khauthanhhieu <thanhhieu021298@gmail.com>
2020-01-02 00:23:54 +0700

    chuyển trang cho trang đăng kí

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,16 +1,58 @@
 import React, { Component } from 'react';
-
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export default class SignUp extends Component {
   constructor() {
     super()
-    this.setState({ page: 0 })
+    this.state = { page: 0, text: '', isTeacher: false }
+    this.handleChange = this.handleChange.bind(this)
+    this.goNextPage = this.goNextPage.bind(this)
+    this.goBackPage = this.goBackPage.bind(this)
+    this.handleChangeRole = this.handleChangeRole.bind(this)
   }
-
+  handleChangeRole() {
+    const currRole = this.state.isTeacher
+    this.setState({ isTeacher: !currRole })
+  }
+  handleChange(value) {
+    this.setState({ text: value })
+  }
+  goNextPage() {
+    const currPage = this.state.page;
+    this.setState({ page: currPage + 1 })
+  }
+  goBackPage() {
+    const currPage = this.state.page;
+    this.setState({ page: currPage - 1 })
+  }
   render() {
-
     const now = new Date()
     const defaultBirth = new Date(now.getFullYear() - 18, now.getMonth() + 1, now.getDate()).toISOString().substr(0, 10)
+    const { page, isTeacher } = this.state;
+
+    const btnNext = (
+      <button type="button" className="btn btn-primary" onClick={this.goNextPage}>
+        Tiếp theo
+      </button>
+    )
+    const btnBlockNext = (
+      <button type="button" className="btn btn-primary btn-block" onClick={this.goNextPage}>
+        Tiếp theo
+      </button>
+    )
+
+    const btnPre = (
+      <button type="button" className="btn btn-light" onClick={this.goBackPage}>
+        Quay lại
+      </button>
+    )
+
+    const btnSubmit = (
+      <button type="submit" className="btn btn-primary">
+        Hoàn tất
+      </button>
+    )
 
     const firstPage = (
       <>
@@ -38,6 +80,12 @@ export default class SignUp extends Component {
           </select>
         </div>
 
+        {btnBlockNext}
+      </>
+    )
+
+    const addressPage = (
+      <>
         <div className="form-group">
           <label>Tên đăng nhập</label>
           <input type="text" className="form-control" placeholder="anguyen" />
@@ -48,14 +96,6 @@ export default class SignUp extends Component {
           <input type="password" className="form-control" />
         </div>
 
-        <button type="button" className="btn btn-primary btn-block">
-          Tiếp theo
-        </button>
-      </>
-    )
-
-    const addressPage = (
-      <>
         <div className="form-group">
           <label>Địa chỉ</label>
           <select id="citySelect" class="form-control mb-3" >
@@ -72,21 +112,20 @@ export default class SignUp extends Component {
 
         <div className="form-group">
           <div className="custom-control custom-checkbox">
-            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+            <input type="checkbox" className="custom-control-input" id="customCheck1" checked={isTeacher} onChange={this.handleChangeRole} />
             <label className="custom-control-label" htmlFor="customCheck1">
               Bạn là giảng viên?
             </label>
           </div>
         </div>
-        <div class="d-flex justify-content-between">
-          <button type="button" className="btn btn-primary">
-            Quay lại
-          </button>
-          <button type="button" className="btn btn-primary">
-            Tiếp theo
-          </button>
-        </div>
 
+        <div class="d-flex justify-content-around">
+          {btnPre}
+          {
+            isTeacher ? btnNext : btnSubmit
+          }
+
+        </div>
       </>
     )
 
@@ -99,27 +138,32 @@ export default class SignUp extends Component {
               <input type="checkbox" className="custom-control-input" id="customCheck1" />
               <label className="custom-control-label" htmlFor="customCheck1">
                 C++
-                </label>
+              </label>
             </div>
           </div>
         </div>
+
         <div className="form-group">
           <label>Tự giới thiệu về bản thân</label>
-          <textarea />
+          <ReactQuill value={this.state.text} onChange={this.handleChange} />
+        </div>
+
+        <div class="d-flex justify-content-around">
+          {btnPre}
+          {btnSubmit}
         </div>
       </>
     )
-
     return (
       <div className="auth-wrapper">
         <div className="auth-inner">
           <form>
             <h3>Đăng ký</h3>
-
-            {/* {firstPage} */}
-            {/* {addressPage} */}
-            {teacherPage}
-
+            {
+              page === 0 ? firstPage :
+                page === 1 ? addressPage :
+                  teacherPage
+            }
             <p className="forgot-password text-right">
               Đã có tài khoản <a href="#">đăng nhập ?</a>
             </p>
